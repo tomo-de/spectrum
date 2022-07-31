fd501af22850659654de72aafbc9a01019465293
tomo-de <71959137+tomo-de@users.noreply.github.com>
2022-07-31 18:15:46 +0900

    [chenge]export時に関数に列番号を直接入れるように変更

--- write_uv-vis-nir.go
+++ write_uv-vis-nir.go
@@ -1,53 +1,59 @@
 package spectrum
 
 import (
+	"fmt"
+
 	"github.com/xuri/excelize/v2"
 )
 
 type Writer interface{
-	WriteColumn(dataName string,data []float64) error
-	SetColumn(column int)
+	WriteColumn(dataName string,data []float64, columnNumber int) error
 }
 type ExcelFile struct{
+	//ファイル名
+	fileName string
+	// fileオブジェクト
 	file *excelize.File
 	// $sheet1など
 	sheet string
-	// 列番号
-	column int
 }
 
 // 波長を読み込みexcelに書き出す
-func (u *UvVisNir) ExportWavelength(e Writer, columnNumber int){
+func (u *UvVisNir) ExportWavelength(e Writer, columnNumber int)error{
 	wavelengths := *u.Wavelengths
-	e.SetColumn(columnNumber)
-	e.WriteColumn("波長", wavelengths)
+	if err := e.WriteColumn("波長", wavelengths, columnNumber); err != nil{
+		return fmt.Errorf("波長の書き出しに失敗 %w", err)
+	}
+	return nil
 }
 
 // 反射強度を読み込みexcelに書き出す
-func (u *UvVisNir) ExportReflectances(e ExcelFile, columnNumber int){
+func (u *UvVisNir) ExportReflectances(e ExcelFile, columnNumber int)error{
 	dataName := *u.DataName
 	reflectances := *u.Reflectances
-	e.SetColumn(columnNumber)
-	e.WriteColumn(dataName, reflectances)
+	if err := e.WriteColumn(dataName, reflectances, columnNumber); err != nil{
+		return fmt.Errorf("反射強度の書き出しに失敗 %w", err)
+	}
+	return nil
 }
 
 
 
 
 // 列をexcelに書き出す
-func (e ExcelFile)WriteColumn(dataName string, data []float64)error{
-    column := e.column
+func (e ExcelFile)WriteColumn(dataName string, data []float64, columnNumber int)error{
+    column := columnNumber
 	f := e.file
 
 	// dataNameを列の最初の行に書き出す
 	// 数値の座標を文字に直す
 	cellPosition, err := excelize.CoordinatesToCellName(column, 1)
 	if err != nil {
-		return err
+		return fmt.Errorf("最初の行の書き出しに失敗 %w", err)
 	}
 	// dataNameを列の最初のcellに入れる
 	if err:= f.SetCellValue(e.sheet, cellPosition, dataName); err!=nil{
-		return err
+		return fmt.Errorf("最初の行の書き出しに失敗 %w", err)
 	}
 
 	// A1,A2などのセルの位置のスライスを作成
@@ -55,7 +61,7 @@ func (e ExcelFile)WriteColumn(dataName string, data []float64)error{
 	for i:=1; i<=len(data); i++ {
 		cellPosition, err := excelize.CoordinatesToCellName(column, i+1)
 		if err != nil{
-			return err
+			return fmt.Errorf("行の書き出しに失敗 %w", err)
 		}
 		cellPositions = append(cellPositions, cellPosition)
 	}
@@ -63,29 +69,40 @@ func (e ExcelFile)WriteColumn(dataName string, data []float64)error{
 	for i:=0; i<len(data); i++ {
 		err:= f.SetCellValue(e.sheet, cellPositions[i], data[i])
 		if err != nil{
-			return err
+			return fmt.Errorf("行の書き出しに失敗 %w", err)
 		}
     }
-		// ブックを保存する
-    if err := f.SaveAs("Book1.xlsx"); err != nil {
-        return err
+
+	// ブックを保存する
+    if err := e.saveFile(); err != nil {
+        return fmt.Errorf("ブックの保存に失敗 %w", err)
 	}
 	return nil
 }
+
+// ブックを保存する
+func (e *ExcelFile)saveFile() error{
+	if err := e.file.SaveAs(e.fileName + ".xlsx"); err != nil {
+         return fmt.Errorf("ブックの保存に失敗 %w", err)
+	}
+	return nil
+}
+
 
 // 現存するexcelのファイルを開く
 func (e *ExcelFile)OpenFile(filePath string) error {
 	var err error
 	e.file, err = excelize.OpenFile(filePath)
 	if err != nil{
-		return err
+		return fmt.Errorf("ファイルの読み込みに失敗 %w", err)
 	}
 	return nil
 }
 
 
 // 新しいファイルを作成する
-func (e *ExcelFile)NewFile(){
+func (e *ExcelFile)NewFile(fileName string){
+	e.fileName = fileName
 	e.file = excelize.NewFile()
 }
 
@@ -103,11 +120,5 @@ func(e ExcelFile)DeleteSheet(sheetName string){
 	//sheetNameのsheetを消す
 	e.file.DeleteSheet(sheetName)
 }
-// 列番号を設定する
-func(e ExcelFile)SetColumn(column int){
-	e.assignColumn(column)
-}
 
-func(e *ExcelFile)assignColumn(column int){
-	e.column = column
-}
+
